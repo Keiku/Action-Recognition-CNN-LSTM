28eeb0aed578eaf308b3369215b3be47006eb56e
Keiku <kuroyanagi.keiichi@gmail.com>
2021-04-20 15:20:31 +0900

    clean up

--- test_on_video.py
+++ test_on_video.py
@@ -1,5 +1,5 @@
 import argparse
-import glob
+import io
 import logging
 import os
 from pathlib import Path
@@ -7,20 +7,48 @@ from pathlib import Path
 import hydra
 import skvideo.io
 import tqdm
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 from PIL import Image, ImageDraw
-from torchvision.utils import make_grid
 
 from data.extract_frames import extract_frames
 from dataset import *
 from models import *
 
 
+class TqdmToLogger(io.StringIO):
+    """
+    Output stream for TQDM which will output to logger module instead of
+    the StdOut.
+    """
+
+    logger = None
+    level = None
+    buf = ""
+
+    def __init__(self, logger, level=None):
+        super(TqdmToLogger, self).__init__()
+        self.logger = logger
+        self.level = level or logging.INFO
+
+    def write(self, buf):
+        self.buf = buf.strip("\r\n\t ")
+
+    def flush(self):
+        self.logger.log(self.level, self.buf)
+
+
 @hydra.main(config_path="./configs", config_name="default.yaml")
-def main(cfg):
+def main(cfg: DictConfig) -> None:
 
     if "experiments" in cfg.keys():
         cfg = OmegaConf.merge(cfg, cfg.experiments)
+
+    # A logger for this file
+    logger = logging.getLogger(__name__)
+
+    # NOTE: hydra causes the python file to run in hydra.run.dir by default
+    logger.info(f"Run script in {HydraConfig.get().run.dir}")
 
     dataset_path = (Path(cfg.dataset.root) / cfg.dataset.frames).as_posix()
     video_path = (
@@ -29,7 +57,7 @@ def main(cfg):
     save_gif_name = Path(cfg.test.video_name).stem
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_shape = (cfg.test.channels, cfg.test.image_dim, cfg.test.image_dim)
+    input_shape = (cfg.test.channels, cfg.test.image_height, cfg.test.image_width)
 
     transform = transforms.Compose(
         [
@@ -42,14 +70,25 @@ def main(cfg):
     labels = sorted(list(set(os.listdir(dataset_path))))
 
     # Define model and load model checkpoint
-    model = CNNLSTM(num_classes=len(labels), latent_dim=cfg.test.latent_dim)
+    model = CNNLSTM(
+        num_classes=cfg.test.num_classes,
+        latent_dim=cfg.test.latent_dim,
+        lstm_layers=cfg.test.lstm_layers,
+        hidden_dim=cfg.test.hidden_dim,
+        bidirectional=cfg.test.bidirectional,
+        attention=cfg.test.attention,
+    )
+    ckpt = torch.load(cfg.test.checkpoint_model, map_location="cpu")
+    model.load_state_dict(ckpt["model"])
     model.to(device)
-    model.load_state_dict(torch.load(cfg.test.checkpoint_model))
     model.eval()
 
     # Extract predictions
     output_frames = []
-    for frame in tqdm.tqdm(extract_frames(video_path), desc="Processing frames"):
+    tqdm_out = TqdmToLogger(logger, level=logging.INFO)
+    for frame in tqdm.tqdm(
+        extract_frames(video_path), file=tqdm_out, desc="Processing frames"
+    ):
         image_tensor = Variable(transform(frame)).to(device)
         image_tensor = image_tensor.view(1, 1, *image_tensor.shape)
 
@@ -66,7 +105,7 @@ def main(cfg):
 
     # Create video from frames
     writer = skvideo.io.FFmpegWriter(f"{save_gif_name}.gif")
-    for frame in tqdm.tqdm(output_frames, desc="Writing to video"):
+    for frame in tqdm.tqdm(output_frames, file=tqdm_out, desc="Writing to video"):
         writer.writeFrame(np.array(frame))
     writer.close()
 
